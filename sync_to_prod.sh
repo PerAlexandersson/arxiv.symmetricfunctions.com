46cfcb059d48c1615121886c0d61febdf7f114c2
Per Alexandersson <per.w.alexandersson@gmail.com>
2026-03-12 21:56:30 +0100

    Fix deploy: also copy static files to public_html/static (nginx serves from there, not src/static)

--- sync_to_prod.sh
+++ sync_to_prod.sh
@@ -79,6 +79,11 @@ else
         "$REMOTE_HOST:~/$REMOTE_PATH/src/static/"
 
     scp -P "$REMOTE_PORT" \
+        "$SCRIPT_DIR/src/static/style.css" \
+        "$SCRIPT_DIR/src/static/utils.js" \
+        "$REMOTE_HOST:~/$REMOTE_PATH/public_html/static/"
+
+    scp -P "$REMOTE_PORT" \
         "$SCRIPT_DIR/src/templates/"*.html \
         "$REMOTE_HOST:~/$REMOTE_PATH/src/templates/"
     scp -P "$REMOTE_PORT" \
